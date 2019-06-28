0136969ce5cd8922dd0b3863d76912962bf4190e
haozhang96 <haozhang@gmx.com>
2019-06-28 07:28:24 -0400

    Prepare to validate fingerprints against schema

--- ext/gatekeeper_frontend/src/server/entities/fingerprint.ts
+++ ext/gatekeeper_frontend/src/server/entities/fingerprint.ts
@@ -2,7 +2,14 @@
 import * as $JSONSchema from "jsonschema";
 import * as $TypeORM from "typeorm";
 
-const validator: $JSONSchema.Validator = new $JSONSchema.Validator();
+// Define the schema to validate fingerprint objects against.
+const schema: $JSONSchema.Schema = {
+    id: "/Fingerprint",
+    properties: {
+
+    },
+    type: "object",
+};
 
 /**
  * Save a fingerprint for the user attached to a given session ID.
@@ -11,10 +18,20 @@ const validator: $JSONSchema.Validator = new $JSONSchema.Validator();
  * @param fingerprint The fingerprint to save
  */
 export function saveFingerprint(sessionID: string, fingerprint: object): void {
-    // Perform validation.
-    
+    // Validate the fingerprint against the JSON schema.
+    const validator: $JSONSchema.Validator = new $JSONSchema.Validator();
+    const validationResult: $JSONSchema.ValidatorResult =
+        validator.validate(fingerprint, schema);
 
-    return;
+    if (!validationResult.valid) {
+        console.warn(
+            "Fingerprint validation error:",
+            validationResult.errors,
+        );
+        return;
+    }
+
+
 }
 
 // Define the entity.
