3a704c2b5a8a0706cb8276c796bb154ca89673fd
haozhang96 <haozhang@gmx.com>
2019-08-03 11:54:18 -0400

    Fix getAllFilePaths error on non-existent root

--- src/utilities/file_system.ts
+++ src/utilities/file_system.ts
@@ -3,6 +3,10 @@ import * as $FileSystem from "fs";
 import * as $Path from "path";
 
 export function getAllFilePaths(root: string): string[] {
+    if (!$FileSystem.existsSync(root)) {
+        return [];
+    }
+
     return $FileSystem.readdirSync(root).map((file) =>
         $Path.resolve(root, file),
     ).map((file) =>
